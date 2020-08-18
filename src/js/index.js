5c9db5722f1a2ddd014d734f742802786b84e87b
Denisacademy <brokenhandq3@yandex.ru>
2020-08-19 00:15:04 +0300

    search part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import Search from './models/Search';
-
+import * as searchView from './views/searchView'
+import { elements } from './views/base';
+//console.log(elements);
 //Global State
 /**
  -Search object
@@ -8,35 +10,40 @@ import Search from './models/Search';
 *-Liked recipes
 **/
 
-const state = {
-
-};
+const state = {};
 
 const controllerSearch = async () => {
   // 1) get query from view
-  const query = 'pizza'; //todo
+  const query = searchView.getInput(); //todo
+  console.log(query);
 
   if(query) {
     // 2) new Search obj and add to state
     state.search = new Search(query);
     
     // 3) Prepare UI for res
+    searchView.clearInput();
+    searchView.clearResults();
     
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) render result on UI
-    console.log(state.search.result);
+    //console.log(state.search.result);
+    console.log(state);
+    //state.forEach(el => console.log(el));
+    searchView.renderResults(state.search.result);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
-  e.preventDefault();//resest stand behave
+//onsole.log(state)
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();//reset standart behave
   controllerSearch();
 
 });
 
-console.log(state);
 //console.log(new Search('pizza').getResults());
 
 
